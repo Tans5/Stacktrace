a8df742b2cb5c8cdeee56044e753ade82e5bc368
tanpengcheng <tanpengcheng@inke.cn>
2023-11-24 12:11:55 +0800

    Fix thread name get error.

--- app/src/main/java/com/tans/stacktrace/MainActivity.kt
+++ app/src/main/java/com/tans/stacktrace/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.annotation.Keep
 import androidx.core.content.getSystemService
 import com.tans.stacktrace.databinding.ActivityMainBinding
 import java.io.File
+import java.io.FileReader
 import java.lang.StringBuilder
 
 
@@ -39,8 +40,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.nativeCrashNewThreadBt.setOnClickListener {
             Thread({
-                testCrash()
-            }, "TestCrashTread").start()
+                   testCrash()
+            }, "TestCrashThread").start()
         }
     }
 
@@ -80,19 +81,31 @@ class MainActivity : AppCompatActivity() {
             time: Long,
             stacks: Array<String>
         ) {
+            val crashThreadName = getThreadNameByTid(tid)
             val crashThread = findThreadByTid(tid)
             val currentProcessInfo = getCurrentProcess()
-            val title = "Fatal signal $sig ($sigName), code $sigSub ($sigSubName) in tid $tid (${ crashThread?.name ?: "Unknown" }), pid ${Process.myPid()} (${currentProcessInfo?.processName ?: "Unknown"})"
+            val title = "Fatal signal $sig ($sigName), code $sigSub ($sigSubName) in tid $tid ($crashThreadName), pid ${Process.myPid()} (${currentProcessInfo?.processName ?: "Unknown"})"
             Log.d(TAG, title)
             for (s in stacks) {
                 Log.d(TAG, s)
             }
-            val javaStackTraces = Thread.getAllStackTraces().toList().find { it.first == crashThread }?.second
+            val javaStackTraces = crashThread?.stackTrace
             val javaStacks = javaStackTraces?.map {
                 "at $it"
             } ?: emptyList()
             for (s in javaStacks) {
                 Log.d(TAG, s)
+            }
+        }
+
+        private fun getThreadNameByTid(tid: Int): String {
+            val file = File("/proc/$tid/comm")
+            return try {
+                FileReader(file).use {
+                    it.readText()
+                }.trim()
+            } catch (e: Throwable) {
+                "Unknown"
             }
         }
 
@@ -102,7 +115,8 @@ class MainActivity : AppCompatActivity() {
                 // MainThread
                 Looper.getMainLooper().thread
             } else {
-                Thread.getAllStackTraces().keys.find { it.id == tid.toLong() }
+                val name = getThreadNameByTid(tid)
+                getAllThread().find { it.name == name }
             }
         }
 
@@ -111,6 +125,24 @@ class MainActivity : AppCompatActivity() {
             val am = app.getSystemService<ActivityManager>()
             val pid = Process.myPid()
             return am?.runningAppProcesses?.find { it.pid == pid }
+        }
+
+        private fun getAllThread(): Array<Thread> {
+            var group = Looper.getMainLooper().thread.threadGroup
+            var topGroup = group
+            while (group != null) {
+                topGroup = group
+                group = topGroup.parent
+            }
+            return if (topGroup != null) {
+                val activeCount = topGroup.activeCount()
+                val threads: Array<Thread?> = Array(activeCount * 2) { null }
+                val realCount = topGroup.enumerate(threads)
+                System.arraycopy(threads, 0, threads, 0, realCount)
+                threads.mapNotNull { it }.toTypedArray()
+            } else {
+                emptyArray()
+            }
         }
     }
 }
